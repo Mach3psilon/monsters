2bc7356a41ba585c33b6d647239fe45a24a723cd
mach3psilon <eray.okutay@invicti.com>
2021-12-11 15:13:21 +0300

    Updates

---
+++ precache-manifest.2686d2fbdda111ba4208045385427eaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a93e8743d77e89f5b404de55857dc9",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "52381fd22b9d48507540",
+    "url": "/monsters/static/css/main.ca5612b4.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52381fd22b9d48507540",
+    "url": "/monsters/static/js/main.fad7f5a9.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
